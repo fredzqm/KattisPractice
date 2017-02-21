1a29275608451bcfb12382acb164dee9f64dedb1
Fred Zhang <fredzqm@gmail.com>
2017-02-21 14:57:55 -0500

    basic test3 works

--- TextProcessor.java
+++ TextProcessor.java
@@ -3,6 +3,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -14,30 +16,15 @@ import java.util.Scanner;
  */
 public class TextProcessor {
 	static Scanner in = new Scanner(System.in);
-	private String str;
-	private int W;
-	private int addIndex;
+	private final String str;
+	private final int W;
+	private int last;
+	private Node root;
+	private LinkedList<Node> acitveNodes;
 
 	public TextProcessor(String str, int W) {
 		this.str = str;
 		this.W = W;
-	}
-
-	public static void main(String[] args) {
-		in.hasNext();
-		String str = in.next();
-		int Q = in.nextInt();
-		int W = in.nextInt();
-		int[] ques = new int[Q];
-		for (int i = 0; i < Q; i++) {
-			ques[i] = in.nextInt();
-		}
-		TextProcessor textProcessor = new TextProcessor(str, W);
-
-		long[] x = textProcessor.solve(ques);
-
-		for (long l : x)
-			System.out.println(l);
 	}
 
 	public long[] solve(int[] ques) {
@@ -55,26 +42,49 @@ public class TextProcessor {
 
 		Iterator<Answer> itr = answers.iterator();
 		Answer nextToSolve = itr.next();
-		Root root = new Root();
-		// LinkedList<Node> nodes = new LinkedList<>();
-		for (addIndex = 0; addIndex < str.length(); addIndex++) {
-			long count = root.process();
 
-			// printTree(root, i);
-			if (addIndex == nextToSolve.end) {
+		// initialize
+		acitveNodes = new LinkedList<>();
+		last = 0;
+		root = new Node();
+		long count = 0;
+		long leafCount = 0;
+		while (last < str.length()) {
+			// remove
+			if (last >= W) {
+				Node n = acitveNodes.remove();
+				count -= n.removeAndGetLength();
+				leafCount--;
+			}
+			// update interior nodes
+			ListIterator<Node> nitr = acitveNodes.listIterator();
+			while (nitr.hasNext()) {
+				Node n = nitr.next();
+				if (!n.isLeaf()) {
+					n = n.advance();
+					nitr.set(n);
+					if (n.isLeaf())
+						leafCount++;
+				}
+			}
+			// add new node from root
+			Node n = root.advance();
+			acitveNodes.add(n);
+			if (n.isLeaf())
+				leafCount++;
+			count += leafCount;
+			System.out.println(this);
+			if (last == nextToSolve.end) {
 				nextToSolve.value = count;
 				while (itr.hasNext()) {
 					nextToSolve = itr.next();
-					if (addIndex == nextToSolve.end)
+					if (last == nextToSolve.end)
 						nextToSolve.value = count;
 					else
 						break;
 				}
-				if (addIndex + W < nextToSolve.end) {
-					root = new Root();
-					addIndex = nextToSolve.end - W;
-				}
 			}
+			last++;
 		}
 
 		Collections.sort(answers, new Comparator<Answer>() {
@@ -90,104 +100,74 @@ public class TextProcessor {
 		return ans;
 	}
 
-	private static void printTree(Root root, int i) {
-		System.out.println("Index: " + i);
-		System.out.print(" Trees:\n");
-		System.out.println(root.toString());
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Index: " + last + "\n");
+		sb.append("Trees: \n" + root.toString());
+		return sb.toString();
 	}
 
 	class Node {
-		private int start, end;
-		private Map<Character, Node> map = new HashMap<>(1);
-		private boolean active = true;
+		private int start;
+		private Map<Character, Node> map;
 
+		/**
+		 * create a root
+		 */
 		public Node() {
-			start = addIndex + 1;
-			end = addIndex;
+			map = new HashMap<>();
 		}
 
-		public int getStart() {
-			return start;
+		/**
+		 * create a leaf
+		 * 
+		 * @param start
+		 */
+		public Node(int start) {
+			this.start = start;
 		}
 
-		public void setStart(int v) {
-			if (start != v)
-				start = v;
-			start = v;
+		/**
+		 * create an interior node
+		 * 
+		 * @param start
+		 * @param node
+		 */
+		public Node(int start, Node node) {
+			this.start = start;
+			map = new HashMap<>();
+			map.put(node.getStartChar(), node);
 		}
 
-		public int getEnd() {
-			return end;
+		public long removeAndGetLength() {
+			int len = last - start;
+			return len;
 		}
 
-		public void incrementStart() {
-			start++;
+		public Node advance() {
+			char toBeAdd = str.charAt(last);
+			if (map.containsKey(toBeAdd)) {
+				Node e = map.get(toBeAdd);
+				if (e.isLeaf()) {
+					e.start++;
+					e = new Node(e.start - 1, e);
+				}
+				map.put(toBeAdd, e);
+				return e;
+			} else {
+				Node n = new Node(last);
+				map.put(toBeAdd, n);
+				return n;
+			}
+		}
+
+		public boolean isLeaf() {
+			return map == null;
 		}
 
 		public char getStartChar() {
-			return str.charAt(getStart());
-		}
-
-		public int size() {
-			return getEnd() - getStart() + 1;
-		}
-
-		public void setActive(boolean x) {
-			active = x;
-		}
-
-		public long process(int level) {
-			level -= 1 + size();
-			if (level <= 0)
-				return 0;
-			long ct = 0;
-			Iterator<Map.Entry<Character, Node>> it = map.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry<Character, Node> entry = it.next();
-				Node n = entry.getValue();
-				long c = n.process(level);
-				if (c == 0) {
-					it.remove();
-				} else {
-					ct += c;
-				}
-			}
-			ct += 1 + size();
-
-			char toBeAdd = str.charAt(addIndex);
-			if (active) {
-				if (map.containsKey(toBeAdd)) {
-					Node e = map.get(toBeAdd);
-					if (e.size() == 0) {
-						e.setActive(true);
-						setActive(false);
-					} else if (map.size() == 1) {
-						map.remove(toBeAdd);
-						setStart(getStart() - getEnd() + addIndex - 1);
-						end = addIndex;
-						map.put(e.getStartChar(), e);
-						e.incrementStart();
-					} else {
-						Node x = new Node();
-						map.put(toBeAdd, x);
-						x.map.put(e.getStartChar(), e);
-						e.incrementStart();
-						setActive(false);
-					}
-				} else {
-					if (map.size() == 0) { // leaf
-						setStart(getStart() - getEnd() + addIndex - 1);
-						end = addIndex;
-					} else { // create leaf
-						setActive(false);
-						map.put(toBeAdd, new Node());
-					}
-					ct++;
-				}
-			}
-			if (!active && map.size() == 0)
-				return 0;
-			return ct;
+			return str.charAt(start);
 		}
 
 		@Override
@@ -197,80 +177,21 @@ public class TextProcessor {
 
 		public String toString(String prefix) {
 			StringBuilder sb = new StringBuilder();
-			Iterator<Map.Entry<Character, Node>> it = map.entrySet().iterator();
-			for (int i = getStart(); i <= getEnd(); i++) {
-				sb.append(str.charAt(i));
-				prefix += " ";
-			}
-			prefix += " ";
-			if (active) {
-				sb.append('*');
-			} else {
-				sb.append(' ');
-			}
-			if (it.hasNext()) {
-				Map.Entry<Character, Node> entry = it.next();
-				sb.append(entry.getKey() + entry.getValue().toString(""));
-				prefix += " ";
-				while (it.hasNext()) {
-					entry = it.next();
-					sb.append("\n" + prefix + entry.getKey() + entry.getValue().toString(prefix));
-				}
-			}
-			return sb.toString();
-		}
-	}
-
-	class Root {
-		private Map<Character, Node> map = new HashMap<>(1);
-
-		public long process() {
-			long ct = 0;
-			Iterator<Map.Entry<Character, Node>> it = map.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry<Character, Node> entry = it.next();
-				Node n = entry.getValue();
-				long c = n.process(W);
-				if (c == 0) {
-					it.remove();
-				} else {
-					ct += c;
-				}
-			}
-
-			char toBeAdd = str.charAt(addIndex);
-			if (map.containsKey(toBeAdd)) {
-				Node e = map.get(toBeAdd);
-				if (e.size() == 0) {
-					e.setActive(true);
-				} else {
-					Node x = new Node();
-					map.put(toBeAdd, x);
-					x.map.put(e.getStartChar(), e);
-					e.incrementStart();
+			if (!isLeaf()) {
+				sb.append(getStartChar());
+				Iterator<Map.Entry<Character, Node>> it = map.entrySet().iterator();
+				if (it.hasNext()) {
+					Map.Entry<Character, Node> entry = it.next();
+					sb.append(" " + entry.getValue().toString(""));
+					prefix += "  ";
+					while (it.hasNext()) {
+						entry = it.next();
+						sb.append("\n" + prefix + entry.getValue().toString(prefix));
+					}
 				}
 			} else {
-				map.put(toBeAdd, new Node());
-				ct++;
-			}
-			return ct;
-		}
-
-		@Override
-		public String toString() {
-			return " " + toString("");
-		}
-
-		public String toString(String prefix) {
-			StringBuilder sb = new StringBuilder();
-			Iterator<Map.Entry<Character, Node>> it = map.entrySet().iterator();
-			if (it.hasNext()) {
-				Map.Entry<Character, Node> entry = it.next();
-				sb.append(entry.getKey() + entry.getValue().toString(""));
-				prefix += " ";
-				while (it.hasNext()) {
-					entry = it.next();
-					sb.append("\n" + prefix + entry.getKey() + entry.getValue().toString(prefix));
+				for (int i = start; i <= last; i++) {
+					sb.append(str.charAt(i));
 				}
 			}
 			return sb.toString();
@@ -286,5 +207,22 @@ public class TextProcessor {
 			this.end = end;
 			this.index = i;
 		}
+	}
+
+	public static void main(String[] args) {
+		in.hasNext();
+		String str = in.next();
+		int Q = in.nextInt();
+		int W = in.nextInt();
+		int[] ques = new int[Q];
+		for (int i = 0; i < Q; i++) {
+			ques[i] = in.nextInt();
+		}
+		TextProcessor textProcessor = new TextProcessor(str, W);
+
+		long[] x = textProcessor.solve(ques);
+
+		for (long l : x)
+			System.out.println(l);
 	}
 }
